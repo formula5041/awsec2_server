eedbe462398a39b4305607eb09599c621cf19b98
formula5041 <formula5041@gmail.com>
2022-05-16 00:47:48 +0800

    [update][後端設定連接前端post程式]

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
+const router = express.Router();
+
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
@@ -14,6 +16,7 @@ app.use(function (req, res, next) {
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Content-Type', 'application/json');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
@@ -32,10 +35,16 @@ app.use(express.static(__dirname + '/travel_write'));
 
 // MONGODB SETTING
 // May only be exist once in app
-mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlParser: true });
+mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlParser: true })
+.then(res=>{
+    console.log("Open connection to mongoDB!!!")
+})
+.catch(err=>{
+    console.log("ERROR CONNECTION");
+    console.log(err)
+})
 
-
-// table => members
+// TABLE => members
 const Schema = mongoose.Schema;
 const memberSchema = new Schema({
     firstName: String,
@@ -43,6 +52,7 @@ const memberSchema = new Schema({
 });
 const Member = mongoose.model("member", memberSchema);
 
+//Read
 app.get('/members', (req, res) => {
     Member.find({}, "firstName lastName").then(members => {
         if (members !== null && members.length > 0) {
@@ -54,17 +64,19 @@ app.get('/members', (req, res) => {
     });
 })
 
-// table => articles
+// TABLE => articles
 const articlesSchema = new Schema({
     title: String,
     content: String,
+    type: String,
     build_t: String,
     update_t: String,
 });
 const Articles = mongoose.model("articles", articlesSchema);
 
-app.get('/articles', (req, res) => {
-    Articles.find({}, "title content build_t update_t").then(articles => {
+//Read
+app.get('/articles/read', (req, res) => {
+    Articles.find({}, "title content type build_t update_t").then(articles => {
         if (articles !== null && articles.length > 0) {
             res.write(JSON.stringify(articles));
         } else {
@@ -72,4 +84,36 @@ app.get('/articles', (req, res) => {
         }
         res.end();
     });
+})
+
+// //Create
+// app.get('/articles/create', (req, res) => {
+//     Articles.create(
+//         {
+//             title:"The First of Keycchron K8",
+//             content:"今天逛三創的時候剛好經過一間包包店，突然發現網路上廣告打超兇的Keychron實體販售店。立馬試打了一下現場的鍵盤，發現實在是太有機械感了，對於第一把機械鍵盤來說，真的很適合入手！",
+//             type:"life",
+//             build_t:"2022-05-15 11:55:00",
+//             update_t:""
+//         },
+//     )
+//     .then(data=>{
+//         console.log("It's worked!");
+//         console.log(data);
+//         res.write(JSON.stringify(data));
+//         res.end();
+//     })
+//     .catch(err=>{
+//         console.log(err);
+//     })
+// })
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.post('/articles/create', (req, res) =>{
+    // res.send("It's work!")
+    // console.log(req.params)
+    const data = req.body.params
+    console.log(data);
+    res.status(200).send(`created article!`)
+    res.end()
 }).listen(8000);
