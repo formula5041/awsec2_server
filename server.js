3a63809962d57521278497a16f1940c799914081
formula5041 <formula5041@gmail.com>
2022-05-22 10:25:24 +0800

    [added][install swagger]

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const mongoose = require('mongoose');
+const express = require('express')
+const path = require('path')
+const app = express()
+const mongoose = require('mongoose')
 
-
-// __dirname === /home/ec2-user
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-app.use(express.static(__dirname + '/travel_write'));
+app.use(express.static(__dirname + '/travel_write'))
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888')
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Content-Type', 'application/json')
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
@@ -40,7 +35,7 @@ mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlPar
     console.log("Open connection to mongoDB!!!")
 })
 .catch(err=>{
-    console.log("ERROR CONNECTION");
+    console.log("ERROR CONNECTION")
     console.log(err)
 })
 
@@ -50,17 +45,17 @@ const memberSchema = new Schema({
     firstName: String,
     lastName: String
 });
-const Member = mongoose.model("member", memberSchema);
+const Member = mongoose.model("member", memberSchema)
 
 //Read
 app.get('/members', (req, res) => {
     Member.find({}, "firstName lastName").then(members => {
         if (members !== null && members.length > 0) {
-            res.write(JSON.stringify(members));
+            res.write(JSON.stringify(members))
         } else {
-            res.write("No members found");
+            res.write("No members found")
         }
-        res.end();
+        res.end()
     });
 })
 
@@ -78,11 +73,11 @@ const Articles = mongoose.model("articles", articlesSchema);
 app.get('/articles/read', (req, res) => {
     Articles.find({}, "title content type build_t update_t").then(articles => {
         if (articles !== null && articles.length > 0) {
-            res.write(JSON.stringify(articles));
+            res.write(JSON.stringify(articles))
         } else {
-            res.write("No articles found");
+            res.write("No articles found")
         }
-        res.end();
+        res.end()
     });
 })
 
@@ -99,13 +94,13 @@ app.post('/articles/create', (req, res) =>{
         update_t:""
     })
     .then(resData=>{
-        console.log("Create it's worked!");
-        console.log(resData);
+        console.log("Create it's worked!")
+        console.log(resData)
         res.status(200).send(`created article!: ${resData}`)
         res.end();
     })
     .catch(err=>{
-        console.log(err);
+        console.log(err)
     })
 })
 
@@ -147,4 +142,4 @@ app.post('/articles/delete', (req, res) =>{
     .catch(err=>{
         console.log(err)
     })
-}).listen(8000);
+}).listen(8000)
