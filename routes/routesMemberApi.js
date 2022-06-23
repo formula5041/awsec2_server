7d45e9db2b618bf52074de15e1b01ac8d67524d3
formula5041 <formula5041@gmail.com>
2022-06-23 22:10:47 +0800

    [added][增加member api與引入 js-base64]

--- routes/routesMemberApi.js
+++ routes/routesMemberApi.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { route } = require('express/lib/application');
+const { decode } = require('js-base64')
 let router = express.Router()
 const mongoose = require('mongoose')
 
@@ -7,8 +8,8 @@ const mongoose = require('mongoose')
 // TABLE => members
 const Schema = mongoose.Schema;
 const memberSchema = new Schema({
-    firstName: String,
-    lastName: String
+    userName: String,
+    password: String
 });
 const Member = mongoose.model("member", memberSchema)
 
@@ -16,7 +17,7 @@ const Member = mongoose.model("member", memberSchema)
 router
  .route('/')
     .get((req, res) => {
-        Member.find({}, "firstName lastName").then(members => {
+        Member.find({}, "userName password").then(members => {
             if (members !== null && members.length > 0) {
                 res.write(JSON.stringify(members))
             } else {
@@ -25,4 +26,31 @@ router
             res.end()
         });
     })
+//get compare to login
+router
+    .route('/login') 
+        .post((req, res) =>{
+            let clintData = req.body.params
+            let theMemberData
+            Member.find({}, "userName password").then(members => {
+                if (members !== null && members.length > 0) {
+                    theMemberData = members[0]
+                    if(theMemberData.userName === clintData.userName) {
+                        if(theMemberData.password === decode(clintData.password)) {
+                            res.send("SUCCESS!")
+                        } else {
+                            res.status(400);
+                            res.send("PASSWORD IS WRONG!")
+                        }
+                    } else {
+                        res.status(400);
+                        res.send("USER NAME IS NOT FOUND!")
+                    }
+                } else {
+                    res.status(204);
+                    res.send("DB MEMBER IS EMPTY!")
+                }
+                res.end()
+            })
+        })
 module.exports = router
